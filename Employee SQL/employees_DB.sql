10db27292bfc0de6ade49eccd46052b6a85792ca
tgrishanina <tatyanagrishanina@MacBook-Air-2.local>
2024-07-29 09:18:47 -0700

    added text to ReadMe

--- Employee SQL/employees_DB.sql
+++ Employee SQL/employees_DB.sql
@@ -36,8 +36,8 @@ CREATE TABLE "dept_manager" (
 CREATE TABLE "dept_emp" (
     "emp_no" INTEGER   NOT NULL,
     "dept_no" VARCHAR   NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE "salaries" (
